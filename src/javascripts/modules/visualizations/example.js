780d50b9f4b648c7cf775b43e7538372f3563606
Chris Williams <cwilliams@miamiherald.com>
2016-10-06 11:41:07 -0400

    fixed am/pm issue

--- src/javascripts/modules/visualizations/example.js
+++ src/javascripts/modules/visualizations/example.js
@@ -6,7 +6,13 @@ class Projection {
   constructor(data) {
     this.hurricane = data.currenthurricane[0];
     this.currentData = this.hurricane.Current;
-    this.time = `${this.currentData.Time.civil.replace(":00 AM", "")} a.m.`;
+    if (this.currentData.Time.ampm === `AM`) {
+      this.time = `${this.currentData.Time.civil.replace(`:00 AM`, ``)} a.m.`;
+    } else if (this.currentData.Time.ampm === `PM`) {
+      this.time = `${this.currentData.Time.civil.replace(`:00 PM`, ``)} p.m.`;
+    } else {
+      this.time = `${this.currentData.Time.civil}`;
+    }
     this.windSpeed = `${this.currentData.WindSpeed.Mph} mph`;
     this.windGust = `${this.currentData.WindGust.Mph} mph`;
     this.movement = `${this.currentData.Movement.Text} at ${this.currentData.Fspeed.Mph} mph`;
@@ -45,8 +51,8 @@ class Projection {
 }
 
 const loadExample = () => {
-  // $.getJSON(`http://api.wunderground.com/api/147b31846a27b039/currenthurricane/forecast/q/FL/Miami.json`, (data) => {
-  $.getJSON(`http://pubsys.miamiherald.com/static/media/projects/2016/hurricane-matthew/data/wu.json`, (data) => {
+  $.getJSON(`http://api.wunderground.com/api/147b31846a27b039/currenthurricane/forecast/q/FL/Miami.json`, (data) => {
+  // $.getJSON(`http://pubsys.miamiherald.com/static/media/projects/2016/hurricane-matthew/data/wu.json`, (data) => {
     new Projection(data).render();
   });
 }
